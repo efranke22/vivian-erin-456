425994c531de015e89dc6df2b88e1c20448dbec2
vivianpowell <vivianajpowell@gmail.com>
2023-04-24 15:19:06 -0500

    working on animated map

--- project/pfasTwinCities/app.R
+++ project/pfasTwinCities/app.R
@@ -13,18 +13,22 @@ rivers <- read_sf('../../data/shp_water_lakes_rivers') %>%
   st_transform(crs = 6783)
 
 pfas7 <- read_csv("../../data/pfas7.csv")
-load("../../data/superfund_site_data/superfund.rds")
 
-superfund_washington <- st_as_sf(superfund %>% filter(county == "Washington", LATITUDE != "NA", LONGITUDE != "NA", LATITUDE >40, LONGITUDE > -94), coords = c("LONGITUDE", "LATITUDE"), crs = 6783) %>%
-  mutate(year = as.numeric(substr(SAMPLE_DATE,7,11)))
+min_action_levels <- data.frame(commonName = c("PFHxS", "PFHxA", "PFOA", "PFBA", "PFBS", "PFOS", "PFPeA"), action_level = c(0.027, NA, 0.035, 7.000, 7.000, 0.027, NA))
 
-pfas_super_washington <- superfund_washington %>%
-  filter(!is.na(commonName))
-  
+pfas_super <- read_csv("../../data/pfas_superfund.csv") %>%
+  filter(LATITUDE != "NA", LONGITUDE != "NA") %>%
+  st_as_sf(coords = c("LONGITUDE", "LATITUDE"), crs = 6783)
 
-#pfas_super_washington <- read_csv("../../data/pfas_superfund.csv") %>%
-#  filter(LATITUDE != "NA", LONGITUDE != "NA", LATITUDE <= 45.298915338179285, LATITUDE >= 44.7471734793455) %>%
-#  st_as_sf(coords = c("LONGITUDE", "LATITUDE"), crs = 6783)
+pfas_health_levels <- pfas7 %>% 
+  left_join(min_action_levels, by = "commonName") %>%
+  mutate(result_num = ifelse(DETECT_FLAG == "Y" & UNIT == "ng/L", `RESULT NUMERIC`/1000, ifelse(DETECT_FLAG == "Y" & UNIT != "ng/L", `RESULT NUMERIC`, 0.00)), above_level = ifelse(DETECT_FLAG == "Y" & result_num > action_level, "Above Health Action Level", ifelse(DETECT_FLAG == "Y" & result_num < action_level, "Below Health Action Level", "Not Detected"))) %>%
+  filter(UNIT != "NA", `FACILITY TYPE` == "Superfund") 
+
+pfas_health_sf <- pfas_health_levels %>%
+  st_as_sf(coords = c("LONGITUDE", "LATITUDE"), crs = 6783)
+
+#LATITUDE <= 45.298915338179285, LATITUDE >= 44.7471734793455
 
 ui <- fluidPage(theme = shinytheme("flatly"), 
                 navbarPage("Water Contamination", tags$style(
@@ -58,42 +62,25 @@ ui <- fluidPage(theme = shinytheme("flatly"),
       }
       "),
                   fluidRow(
-                    column(
-                    h4("Please select a PFAS to view most recent sample results"),
-                    selectInput(inputId = "commonName",
-                      label = "Chemical Name",
-                      choices = unique(superfund_washington$commonName)),
-                    width = 6
-                    ),
-                    column(
                     h4("Please select a PFAS to view time series"),
                     selectInput(inputId = "common_name",
                       label = "Chemical Name",
-                      choices = unique(pfas_super_washington$commonName)),
+                      choices = unique(pfas_super$commonName)),
                     sliderInput(
                       inputId = "year",
                       label = "Sample Year",
-                      min = min(pfas_super_washington$year),
-                      max = max(pfas_super_washington$year),
+                      min = min(pfas_super$year),
+                      max = max(pfas_super$year),
                       sep = "",
                       value = 2005,
                       ticks = FALSE,
                       step = 1,
                       animate = animationOptions(interval = 300, loop = TRUE, playButton = c("Play Animation"))
-                    ),
-                    width = 6
                     )
                   ),
                 fluidRow(
-                  column(
-                  plotOutput(outputId = "pfasplot"), 
-                  width = 6
-                  ),
-                  column(
                   plotOutput(outputId = "pfasplottemporal",
-                             height = 600),
-                  width = 6
-                  )
+                             height = 600)
                   ),
                 fluidRow(
                   p("These data are collected by the MPCA. More information can be found at https://www.pca.state.mn.us/air-water-land-climate/cleaning-up-minnesota-superfund-sites.")
@@ -114,29 +101,12 @@ ui <- fluidPage(theme = shinytheme("flatly"),
 server <- function(input, output) {
   
   counties_cropped <- st_crop(counties, xmin = -93.2, xmax=-92.7, ymin = 44.7, ymax=45.35)
-  output$pfasplot <- renderPlot(
-      ggplot() +
-      geom_sf(data = counties_cropped %>% filter(name == "Washington"), color = "navajowhite", fill = "ivory", size = 1)+
-      geom_sf(data = (superfund_washington %>% filter(commonName == input$commonName, DETECT_FLAG == "Y") 
-                      %>% mutate(over_limit = RESULT_NUMERIC > MIN_ACTION_LEVEL, 
-                                 over_limit = case_when(is.na(over_limit) ~ FALSE, TRUE ~ over_limit))), 
-              size= 0.9, aes(color = RESULT_NUMERIC), alpha = 0.5)+
-        labs(color = "Detected (ug/L)", title = paste("Detected", input$commonName, "in Washington \n County Superfund sites"))+
-        scale_color_gradient(low = "pink", high = "darkred")+
-        theme_classic() +
-        theme(axis.ticks = element_blank(), 
-              axis.text = element_blank(), 
-              axis.line = element_blank(),
-              title = element_text(size = 20),
-              legend.title = element_text(size=16),
-              legend.text = element_text(size=14),
-              text = element_text(family = "AppleGothic")),
-      height = 600
-  )
+  
   output$pfasplottemporal <- renderPlot(
     ggplot() + 
-      geom_sf(data = counties_cropped %>% filter(name == "Washington"), color = "navajowhite", fill = "ivory", size = 1)+
-      geom_sf(data = (pfas_super_washington %>%
+      geom_sf(data = counties_cropped #%>% filter(name == "Washington")
+              , color = "navajowhite", fill = "ivory", size = 1)+
+      geom_sf(data = (pfas_super %>%
                 filter(commonName == input$common_name, year == input$year)), aes(color = perc_detected)) +
       labs(color = "% Samples Above Detection Limit", title = paste(input$common_name, "over time in Washington \n County Superfund sites"))+
       scale_color_gradient(low = "pink", high = "darkred")+
